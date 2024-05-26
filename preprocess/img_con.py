11f1d9265bbea8e03b467e6706d6ee1567d98753
PYLinJasper <jasperpylofficial@gmail.com>
2024-05-25 21:13:59 -0400

    turn into main function to formalize

--- preprocess/img_con.py
+++ preprocess/img_con.py
@@ -7,38 +7,47 @@ import cv2
 import numpy as np
 from tqdm import tqdm
 
-from config import *
+from pre_config import *
 from contour import init_contour_finding, find_contour
 
-# data root path
-img_dir = data_dir + '01_Preprocessed/'
-sto_dir = '02_Contour/'
-create_folder(data_dir + sto_dir)
 
-file_list, file_list_fld = init_contour_finding(img_dir, epidural, intraparenchymal,
-           subarachnoid, intraventricular, multi, subdural, normal)
+def main(args):
+    '''this is the main program'''
+    # data root path
+    data_dir = args.root_path + '/'
+    img_info_dir = data_dir + 'segmentation/'
+    img_dir = data_dir + '01_Preprocessed/'
+    sto_dir = '02_Contour/'
+    create_folder(data_dir + sto_dir)
 
-for file, fld in zip(file_list, file_list_fld):
-    if fld == 'normal':
-        break
+    file_list, file_list_fld = init_contour_finding(img_dir, epidural, intraparenchymal,
+            subarachnoid, intraventricular, multi, subdural, normal)
+
+    for file, fld in zip(file_list, file_list_fld):
+        if fld == 'normal':
+            break
     
-    create_folder(data_dir + sto_dir + fld)
-    for img_name in tqdm(file, desc=f"{fld}", unit="image"):
-        # read image
-        img = cv2.imread(img_dir + fld + '/' + img_name)
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        create_folder(data_dir + sto_dir + fld)
+        for img_name in tqdm(file, desc=f"{fld}", unit="image"):
+            # read image
+            img = cv2.imread(img_dir + fld + '/' + img_name)
+            gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         
-        img_size0 = gray_img.shape[0]
-        img_size1 = gray_img.shape[1]
-        gray_img = gray_img.reshape(img_size0, img_size1, 1)
+            img_size0 = gray_img.shape[0]
+            img_size1 = gray_img.shape[1]
+            gray_img = gray_img.reshape(img_size0, img_size1, 1)
 
-        contour = find_contour(gray_img, img_name)
+            contour = find_contour(gray_img, img_name)
     
-        # Create a mask for the brightest oval-like polygon
-        mask = np.zeros_like(gray_img, dtype=np.uint8)
+            # Create a mask for the brightest oval-like polygon
+            mask = np.zeros_like(gray_img, dtype=np.uint8)
             
-        cv2.drawContours(mask, [contour], 0, 255, thickness=cv2.FILLED)
+            cv2.drawContours(mask, [contour], 0, 255, thickness=cv2.FILLED)
 
-        # outside contour to be black
-        result = cv2.bitwise_and(img, img, mask=mask)
-        cv2.imwrite(data_dir + sto_dir + fld + '/' + img_name, result)
+            # outside contour to be black
+            result = cv2.bitwise_and(img, img, mask=mask)
+            cv2.imwrite(data_dir + sto_dir + fld + '/' + img_name, result)
+
+if __name__ == "__main__":
+    ARGS = get_args()
+    main(ARGS)

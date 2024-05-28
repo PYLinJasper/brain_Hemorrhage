e4737d8645a5c861158da5f8c21dfeb5c2fbc544
PYLinJasper <jasperpylofficial@gmail.com>
2024-05-27 23:55:36 -0400

    add args and variable

--- model/input_config.py
+++ model/input_config.py
@@ -1,6 +1,51 @@
 import os
 import ast
+import argparse
 from random import sample
+
+epidural = 'epidural'
+intraparenchymal = 'intraparenchymal'
+subarachnoid = 'subarachnoid'
+intraventricular = 'intraventricular'
+multi = 'multi'
+subdural = 'subdural'
+subdural_1 = 'subdural_1'
+subdural_2 = 'subdural_2'
+normal = 'normal'
+
+csv = '.csv'
+jpg = '.jpg'
+
+def get_args():
+    """
+    Just get the command line options using argparse
+    @return: Instance of argparse arguments
+    """
+
+    parser_description = 'Root path for the dcms data images'
+    parser = argparse.ArgumentParser(description=parser_description)
+
+    parser.add_argument('-r',
+                        '--root',
+                        # use args.column_to_parse
+                        dest='root_path',
+                        # accepted input type
+                        type=str,
+                        help='root path for images',
+                        # defult value options
+                        default='./dcms')
+    
+    parser.add_argument('-s',
+                        '--size',
+                        # use args.column_to_parse
+                        dest='size',
+                        # accepted input type
+                        type=int,
+                        help='training image size',
+                        # defult value options
+                        default=256)
+
+    return parser.parse_args()
 
 def show(img, fld, mask, dia_gray_img, img_name):
     plt.figure(figsize=(10,5))
